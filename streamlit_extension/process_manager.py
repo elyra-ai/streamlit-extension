440fb23010edab7f02b8fbab997f7173a3ee1695
Alan Chin <akchin@us.ibm.com>
2022-06-09 13:39:20 -0700

    Remove license headers and add logging and other changes

--- streamlit_extension/process_manager.py
+++ streamlit_extension/process_manager.py
@@ -1,68 +1,100 @@
-#
-# Copyright 2022 Elyra Authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 import os
 import sys
 import socket
 from subprocess import Popen
+from subprocess import CalledProcessError
 from subprocess import PIPE
 from traitlets.config import SingletonConfigurable
+from traitlets.config import LoggingConfigurable
 
 
 class StreamlitManager(SingletonConfigurable):
+    """
+    Class to keep track of streamlit application instances and manage lifecycles
+    """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.streamlit_instances = {}
 
-    def start(self, streamlit_app_filepath):
+    def start(self, streamlit_app_filepath: str) -> str:
         streamlit_app = StreamlitApplication(streamlit_app_filepath=streamlit_app_filepath)
         streamlit_app.start()
         self.streamlit_instances[streamlit_app_filepath] = streamlit_app
         return streamlit_app.port
 
-    def stop(self, streamlit_app_filepath):
+    def stop(self, streamlit_app_filepath: str) -> None:
         streamlit_app = self.streamlit_instances.get(streamlit_app_filepath)
-        streamlit_app.stop()
+        if streamlit_app:
+            streamlit_app.stop()
+        else:
+            self.log.info(f"Unable to find running instance of {streamlit_app_filepath} application")
+
+    def restart(self, streamlit_app_filepath: str) -> None:
+        """
+        Forces a restart of a streamlit application.
+        NOTE: does not restart a "stopped" application process
+        :param streamlit_app_filepath:
+        :return:
+        """
+        streamlit_app = self.streamlit_instances.get(streamlit_app_filepath)
+        if streamlit_app:
+            streamlit_app.stop()
+            streamlit_app.start()
+        else:
+            self.log.info(f"Unable to find running instance of {streamlit_app_filepath} application")
 
 
-class StreamlitApplication:
-    def __init__(self, streamlit_app_filepath):
+class StreamlitApplication(LoggingConfigurable):
+    def __init__(self, streamlit_app_filepath: str, **kwargs):
+        """
+        :param streamlit_app_filepath: the relative path to the streamlit application file (*.py)
+        :param kwargs:
+        """
+        super().__init__(**kwargs)
         self.port = get_open_port()
         self.app_start_dir = os.path.dirname(streamlit_app_filepath)
-        self.streamlit_cmd = [sys.executable, "-m", "streamlit", "run", os.path.basename(streamlit_app_filepath),
+        self.app_basename = os.path.basename(streamlit_app_filepath)
+        self.streamlit_cmd = [sys.executable, "-m", "streamlit", "run", self.app_basename,
                               "--browser.gatherUsageStats=false",  # turn off usage upload to streamlit
                               "--server.runOnSave=true",           # auto refresh app on save
                               "--server.headless=true",            # run headless, avoids email sign up
                               "--server.port", self.port]
         self.process = None
 
-    def start(self):
-        self.process = Popen(self.streamlit_cmd, cwd=self.app_start_dir, stdout=PIPE)
+    def start(self) -> None:
+        if not self.process or not self.is_alive():
+            self.log.info(f"Starting Streamlit application '{self.app_basename}' on port {self.port}")
+            try:
+                self.process = Popen(self.streamlit_cmd, cwd=self.app_start_dir, stdout=PIPE)
+            except CalledProcessError as error:
+                self.log.info(f"Failed to start Streamlit application on port {self.port} due to {error}")
 
-        # Voodoo magic, needs to 'hit' the process otherwise server will not serve
-        self.process.stdout.readline()
+            # Voodoo magic, needs to 'hit' the process otherwise server will not serve
+            self.process.stdout.readline()
 
-    def stop(self):
-        self.process.terminate()
+    def stop(self) -> None:
+        if self.process:
+            self.log.info(f"Stopping Streamlit application '{self.app_basename}' on port {self.port}")
+            self.process.terminate()
+            self.process = None
+        else:
+            self.log.info(f"Streamlit application '{self.app_basename}' is not running")
 
-    def is_alive(self):
-        return self.process.poll()
+    def is_alive(self) -> bool:
+        """
+        Check if child process has terminated.
+        """
+        if self.process:
+            return False if self.process.poll() else True
+        else:
+            return False
 
 
 def get_open_port() -> str:
+    """
+    Returns an open port on the application host
+    :return:
+    """
     sock = socket.socket()
     sock.bind(('', 0))
     return str(sock.getsockname()[1])
