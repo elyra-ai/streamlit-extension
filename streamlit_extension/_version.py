012a12122091d8d7465c4c00631a0c2f0588f8ec
Alex Bozarth <ajbozart@us.ibm.com>
2022-08-24 16:17:33 -0500

    Add Apache 2 License
    The cookiecutter added a BSD 3 license, but under the copywrite of
    the developer who uses the cookiecutter. Therefor we are switching
    to the Apache 2 License used by the Elyra org.

--- streamlit_extension/_version.py
+++ streamlit_extension/_version.py
@@ -1,3 +1,19 @@
+#
+# Copyright 2022 Elyra Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import json
 from pathlib import Path
 
@@ -7,7 +23,7 @@ __all__ = ["__version__"]
 def _fetchVersion():
     HERE = Path(__file__).parent.resolve()
 
-    for settings in HERE.rglob("package.json"): 
+    for settings in HERE.rglob("package.json"):
         try:
             with settings.open() as f:
                 version = json.load(f)["version"]
